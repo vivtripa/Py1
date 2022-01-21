e418aab5e514c1f3bfc37976e687ceb33ee888d6
Vivek Tripathi <vtripath@freedommortgage.com>
2022-01-21 13:10:24 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,55 @@
+print('Welcome to our system')
+# name = input('What is your  name? ')
+# birth_year = int(input('Enter your birth year: '))
+# age = 2021 - birth_year
+# print(name,"your age is ",age)
+# #--------------------------------
+# first = float(input('First: '))
+# second = float(input('Second: '))
+#
+# print('Result:',first + second)
+
+# course ='Python for Beginners'
+# print(course.count('o'))
+# print(course.upper())
+# print(course.find('y'))
+# print(course.replace('for','4'))
+# print('Python' in course)
+
+# print(7/3)
+# print(7**3)
+# x=10
+# x+=3
+# print(x)
+# print(7%3)
+
+# x= 3>2
+# print(x)
+
+# temp = 10
+# if temp > 30:
+#     print("It's a hot day ")
+#     print("Drink a lot of water")
+# elif temp > 20:
+#     print("It's not so hot")
+# else: print("It's cold day")
+
+# i = 0
+# while i <= 10:
+#     print(i*'-')
+#     i +=1
+
+# names =['Vivek','Vinay', 'vinod', 'vineeta', 'vibha']
+# print (names[1:4])
+
+# numbers=[2,5,8,9,11]
+# numbers.insert(0,35)
+# print(numbers)
+# print(len(numbers))
+# for item in numbers:
+#     print(item)
+
+for item in range(2, 10, 2):
+    print(item)
+
+    numbers = (1, 2, 3, 4, 5)
---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def multiply(first_number, second_number):
+    print(first_number * second_number)
+
+
+def draw_triangle (height):
+while i < height:  
+
+
+multiply(10, 20)
